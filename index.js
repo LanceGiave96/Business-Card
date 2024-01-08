c598df5f5be42524e8b240d0ffa88ceb77f1e45a
LanceGiave96 <enricopedemonti@hotmail.it>
2024-01-08 20:27:09 +0100

    updating rendering to not overflow the page

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
 let div_skills_el = document.getElementById("div-skills")
+let div_description_el = document.getElementById("div-presentation")
 div_skills_el.style.display = "none"
+div_description_el.style.display = "block"
 
 let button_el = document.getElementById("button-skills")
 
@@ -9,11 +11,13 @@ function showSkills(){
     if(div_skills_el.style.display === "none")
     {
         div_skills_el.style.display = "block"
+        div_description_el.style.display = "none"
         button_el.textContent = "Skills ➖​"
     }
     else
     {
         div_skills_el.style.display = "none"
+        div_description_el.style.display = "block"
         button_el.textContent = "Skills ​➕​"
     }
 
