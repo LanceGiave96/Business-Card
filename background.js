8fe12181bf42502c4bdcd8da28c489a6682587f9
LanceGiave96 <enricopedemonti@hotmail.it>
2024-01-12 20:36:24 +0100

    updating styles

--- background.js
+++ background.js
@@ -23,63 +23,62 @@ export default {
         {
             key: "6",
             url: "https://images.unsplash.com/photo-1464802686167-b939a6910659?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        }/*,
+        },
         {
             key: "7",
-            url: ""
+            url: "https://images.unsplash.com/photo-1675941321153-d5647d12dab4?q=80&w=1167&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "8",
-            url: ""
+            url: "https://images.unsplash.com/photo-1704790024545-d801eb3d3e2e?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "9",
-            url: ""
+            url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "10",
-            url: ""
+            url: "https://images.unsplash.com/photo-1536152470836-b943b246224c?q=80&w=1338&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "11",
-            url: ""
+            url: "https://images.unsplash.com/photo-1499002238440-d264edd596ec?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "12",
-            url: ""
+            url: "https://images.unsplash.com/photo-1516690553959-71a414d6b9b6?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "13",
-            url: ""
+            url: "https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "14",
-            url: ""
+            url: "https://images.unsplash.com/photo-1534684686641-05569203ecca?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "15",
-            url: ""
+            url: "https://wallpapercave.com/wp/wp2329752.jpg"
         },
         {
             key: "16",
-            url: ""
+            url: "https://mrwallpaper.com/images/high/mr-robot-elliot-glitch-art-background-gb7xxlkeskgsf6be.webp"
         },
         {
             key: "17",
-            url: ""
+            url: "https://wallpapercosmos.com/w/middle-retina/8/e/5/22845-3840x2160-desktop-4k-breaking-bad-background-image.jpg"
         },
         {
             key: "18",
-            url: ""
+            url: "https://rare-gallery.com/thumbnail/1323150-The-Sopranos-HD-Wallpaper.jpg"
         },
         {
             key: "19",
-            url: ""
+            url: "https://images.unsplash.com/photo-1543357480-c60d40007a3f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         },
         {
             key: "20",
-            url: ""
+            url: "https://images.unsplash.com/photo-1454372182658-c712e4c5a1db?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
-        */
     ]
 }
--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 
-const div_skills_el = document.getElementById("div-skills")
-const div_description_el = document.getElementById("div-presentation")
-div_skills_el.style.display = "none"
-div_description_el.style.display = "block"
-
-const button_skills = document.getElementById("button-skills")
-button_skills.addEventListener("click",function(){
-    
-    if(div_skills_el.style.display === "none")
-    {
-        div_skills_el.style.display = "block"
-        div_description_el.style.display = "none"
-        button_el.textContent = "Skills ➖​"
-    }
-    else
-    {
-        div_skills_el.style.display = "none"
-        div_description_el.style.display = "block"
-        button_el.textContent = "Skills ​➕​"
-    }
-
-}
-)
-
+//Function to set randomically a image background
 import background from "./background.js"
 const images = background.images
 const button_background = document.getElementById("button-background")
@@ -36,4 +13,30 @@ function getRandomBackground(){
 
 }
 
+//Set random background when site loaded
 getRandomBackground()
+
+//Show/Hide skills/presentation section by clicking button
+const div_skills_el = document.getElementById("div-skills")
+const div_description_el = document.getElementById("div-presentation")
+div_skills_el.style.display = "none"
+div_description_el.style.display = "block"
+
+const button_skills = document.getElementById("button-skills")
+button_skills.addEventListener("click",function(){
+    
+    if(div_skills_el.style.display === "none")
+    {
+        div_skills_el.style.display = "block"
+        div_description_el.style.display = "none"
+        button_skills.textContent = "Skills ➖​"
+    }
+    else
+    {
+        div_skills_el.style.display = "none"
+        div_description_el.style.display = "block"
+        button_skills.textContent = "Skills ​➕​"
+    }
+
+}
+)
