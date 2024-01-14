bbda87e7a4db4f67aa2257e27cc00a7e4cd601b8
LanceGiave96 <enricopedemonti@hotmail.it>
2024-01-14 11:45:57 +0100

    Center image background; changed locally location of image to improve rendering

--- background.js
+++ background.js
@@ -22,63 +22,63 @@ export default {
         },
         {
             key: "6",
-            url: "https://images.unsplash.com/photo-1464802686167-b939a6910659?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_6.jpg"       
         },
         {
             key: "7",
-            url: "https://images.unsplash.com/photo-1675941321153-d5647d12dab4?q=80&w=1167&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_7.jpg"     
         },
         {
             key: "8",
-            url: "https://images.unsplash.com/photo-1704790024545-d801eb3d3e2e?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_8.jpg"     
         },
         {
             key: "9",
-            url: "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_9.jpg"     
         },
         {
             key: "10",
-            url: "https://images.unsplash.com/photo-1536152470836-b943b246224c?q=80&w=1338&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_10.jpg"     
         },
         {
             key: "11",
-            url: "https://images.unsplash.com/photo-1499002238440-d264edd596ec?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_11.jpg"     
         },
         {
             key: "12",
-            url: "https://images.unsplash.com/photo-1516690553959-71a414d6b9b6?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_12.jpg"     
         },
         {
             key: "13",
-            url: "https://images.unsplash.com/photo-1511208687438-2c5a5abb810c?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_13.jpg"     
         },
         {
             key: "14",
-            url: "https://images.unsplash.com/photo-1534684686641-05569203ecca?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_14.jpg"     
         },
         {
             key: "15",
-            url: "https://wallpapercave.com/wp/wp2329752.jpg"
+            url: "images/background_15.jpg"  
         },
         {
             key: "16",
-            url: "https://mrwallpaper.com/images/high/mr-robot-elliot-glitch-art-background-gb7xxlkeskgsf6be.webp"
+            url: "images/background_16.jpg"  
         },
         {
             key: "17",
-            url: "https://wallpapercosmos.com/w/middle-retina/8/e/5/22845-3840x2160-desktop-4k-breaking-bad-background-image.jpg"
+            url: "images/background_17.jpg"  
         },
         {
             key: "18",
-            url: "https://rare-gallery.com/thumbnail/1323150-The-Sopranos-HD-Wallpaper.jpg"
+            url: "images/background_18.jpg"  
         },
         {
             key: "19",
-            url: "https://images.unsplash.com/photo-1543357480-c60d40007a3f?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_19.jpg"  
         },
         {
             key: "20",
-            url: "https://images.unsplash.com/photo-1454372182658-c712e4c5a1db?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            url: "images/background_14.jpg"  
         }
     ]
 }
